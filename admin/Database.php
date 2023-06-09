f9483d3ef1f556333ffb0e4915fd17db5819bd81
fzmubin98 <71100433+fzmubin98@users.noreply.github.com>
2023-06-09 22:00:42 +0600

    Update Database.php changed servername to RDS endpoint

--- admin/Database.php
+++ admin/Database.php
@@ -1,7 +1,7 @@
 <?php
-$servername='localhost';
+$servername='fazle-db.c5tsce7xtv8b.eu-west-3.rds.amazonaws.com';
 $username='root';
-$password='';
+$password='bjit1234';
 $dbname = "moviebook";
 $conn=mysqli_connect($servername,$username,$password,"$dbname");
 if(!$conn){
