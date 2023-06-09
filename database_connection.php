08227614875deaa9a1e30308a46a803aa7b916cb
fzmubin98 <71100433+fzmubin98@users.noreply.github.com>
2023-06-09 21:58:13 +0600

    Update database_connection.php changed server to RDS endpoint

--- database_connection.php
+++ database_connection.php
@@ -2,6 +2,6 @@
 
 //database_connection.php
 
-$connect = new PDO("mysql:host=192.168.20.158;dbname=moviebook", "root", "");
+$connect = new PDO("mysql:host=fazle-db.c5tsce7xtv8b.eu-west-3.rds.amazonaws.com;dbname=moviebook", "root", "bjit1234");
 
 ?>
