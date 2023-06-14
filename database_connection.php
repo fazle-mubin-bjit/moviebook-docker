b46a8f73b67b2229ab533a09c6ff365826c93b6f
fazle-mubin-bjit <131145969+fazle-mubin-bjit@users.noreply.github.com>
2023-06-14 11:02:21 +0600

    Update database_connection.php changed to db-container

--- database_connection.php
+++ database_connection.php
@@ -2,6 +2,6 @@
 
 //database_connection.php
 
-$connect = new PDO("mysql:host=fazle-db.c5tsce7xtv8b.eu-west-3.rds.amazonaws.com;dbname=moviebook", "root", "bjit1234");
+$connect = new PDO("db-container", "root", "bjit1234");
 
 ?>
